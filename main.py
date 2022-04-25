9cdd410957e90698c4d4be21d3dd5c224bb0cb54
Dylan <ViableStream808@gmail.com>
2022-04-25 14:14:56 -0700

    update strat

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from sources.Decks import Decks
 from sources.Game import Game
 
 random.seed(42)
-NUM_SIMULATION = 100
+NUM_SIMULATION = 2_500
 
-game = Game(["RAND"]*4)
+game = Game(["GREEDY_SIMPLE"]*2+["GREEDY_HIST_RAND"]*2)
 for n in range(NUM_SIMULATION + 1):
     game.initialize_game()
     game.play()
+    
 x = game.print_game_record()
 
 
--- sources/Strategy.py
+++ sources/Strategy.py
@@ -34,6 +34,7 @@ def discard_no_history(hand, history = None, wanted_card = None):
         discard_candidates += [card]
     return random.choice(discard_candidates)
 
+### POSITIVE MEMORY ###
 def discard_history_basic(hand, history, wanted_card = None):
     discard_candidates = []
 
@@ -93,14 +94,75 @@ def discard_history_wild(hand, history, wanted_card = None):
         return discard_no_history(hand, history = None, wanted_card = wanted_card)
     else:
         return discard_history_basic(hand, history, wanted_card)
+    
+### NEGATIVE MEMORY ###
+def discard_history_basic_N(hand, history, wanted_card = None):
+    discard_candidates = []
 
+    # Keep card if wanted or NOT in history
+    for card in hand:
+        if card == wanted_card:
+            continue
+        if card not in history:
+            continue
+        discard_candidates += [card]
+
+    # In case no cards are candidates
+    if discard_candidates == []:
+        return discard_no_history(hand, wanted_card)
+
+    return random.choice(discard_candidates)
+
+def discard_history_random_N(hand, history, wanted_card = None):
+    discard_candidates = []
+
+    # More recent in memory == less likely to keep
+    for card in hand:
+        if card == wanted_card:
+            continue
+        if card in history:
+            if random.random() > 1/(PLAYER_MEMORY - history.index(card))**0.5:
+                continue
+        discard_candidates += [card]
+
+    # In case no cards are candidates
+    if discard_candidates == []:
+        return discard_no_history(hand, wanted_card)
+
+    return random.choice(discard_candidates)
+
+def discard_history_freq_N(hand, history, wanted_card = None):
+    discard_candidates = []
+    freq = Counter(history)
+
+    # Keep cards in history that have been seen a lot
+    for card in hand:
+        if card == wanted_card:
+            continue
+        if card in history:
+            if freq[card] == 1:
+                continue
+        discard_candidates += [card]
+
+   # In case no cards are candidates
+    if discard_candidates == []:
+        return discard_no_history(hand, wanted_card)
+
+    return random.choice(discard_candidates)
+
+def discard_history_wild_N(hand, history, wanted_card = None):
+    if random.random() < 0.1:
+        return discard_no_history(hand, history = None, wanted_card = wanted_card)
+    else:
+        return discard_history_basic_N(hand, history, wanted_card)
+    
 ### MOVE FUNCTION ###
 def strat(hand, history, next_card, discard_strat = discard_no_history):
     frequency = Counter(hand)
     wanted_card = frequency.most_common(1)[0][0]
     
     hand.append(next_card)
-    discard_card = discard_strat(hand, wanted_card)
+    discard_card = discard_strat(hand, history, wanted_card)
     hand.remove(discard_card)
 
     update_player_history(history, discard_card)  
@@ -111,4 +173,8 @@ decision_dictionary = {"RAND": discard_random,
                        "GREEDY_HIST": discard_history_basic,
                        "GREEDY_HIST_RAND": discard_history_random, 
                        "GREEDY_HIST_FREQ": discard_history_freq,
-                       "GREEDY_HIST_WILD": discard_history_wild}
+                       "GREEDY_HIST_WILD": discard_history_wild,
+                       "GREEDY_HIST_N": discard_history_basic_N,
+                       "GREEDY_HIST_RAND_N": discard_history_random_N, 
+                       "GREEDY_HIST_FREQ_N": discard_history_freq_N,
+                       "GREEDY_HIST_WILD_N": discard_history_wild_N}
